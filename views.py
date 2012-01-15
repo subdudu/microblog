372468a5af597547257a791205c7f6193188d853
David Du <subdudu-git@yahoo.com.cn>
2012-01-15 11:45:17 +0800

    the main page will display differently according to the login state

--- views.py
+++ views.py
@@ -3,11 +3,20 @@ from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponse
 from django.template.context import RequestContext
 from django.template import loader
+from django.contrib.auth.forms import AuthenticationForm
 
 
 
 def home(request):
-	c = RequestContext(request, {
-			'title': u'武警黄金技术学校欢迎您！',
-		})
+	if request.user.is_authenticated():
+		c = RequestContext(request, {
+				'title': u'武警黄金技术学校欢迎您！',
+			})
+	else:
+		form = AuthenticationForm()
+		c = RequestContext(request, {
+				'title': u'武警黄金技术学校欢迎您！',
+				'form': form,
+				'next': '/',
+			})
 	return HttpResponse(loader.get_template('index.html').render(c))
