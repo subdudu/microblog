5c655aec0cb6ce56ab83907c61e5555b30def198
David Du <subdudu-git@yahoo.com.cn>
2012-01-18 17:22:22 +0800

    show all posts in the main page

--- media/js/posts.js
+++ media/js/posts.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 	$.getJSON("/posts/realtime_posts", function(data, status, xhr) {
-		$("#realtime_posts").html(data[0].content);
+		$("#realtime_posts").html(show_posts(data));
 	});
 });
 
-var show_posts = function(posts) {}
+var show_posts = function(posts) {
+	var s = "";
+	for (p in posts) {
+		s += "<div><span>" + posts[p].content + "</span><span>" + posts[p].last_mod_time + "</span></div>";
+	}
+	return s;
+}
