716fa328837fc27723c42c97925921a4fc675abd
David Du <subdudu-git@yahoo.com.cn>
2012-02-13 14:54:54 +0800

    post will be commited async now

--- media/js/posts.js
+++ media/js/posts.js
@@ -7,6 +7,17 @@ $(document).ready(function() {
 		$("#realtime_posts").html(s);
 		setTimeout(add_realtime_posts, 1);
 	});
+	$("#id_commit").click(function(){
+		$.ajax({
+			type: "POST",
+			url: "/posts/add/",
+			data: $("#form_post_ajax").serialize(),
+			success: function(msg) {
+				$("#id_content").val("");
+				alert("提交成功");
+			}
+		});
+	});
 });
 
 function add_realtime_posts() {
